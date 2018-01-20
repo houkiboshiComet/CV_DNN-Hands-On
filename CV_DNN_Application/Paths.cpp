d1f86b342276b57aadab69a2bf36773039fd383d
houkiboshiComet <wish.on.broomystars@gmail.com>
2018-01-21 02:16:13 +0900

    Correct path for initial project setting.

--- CV_DNN_Application/Paths.cpp
+++ CV_DNN_Application/Paths.cpp
@@ -3,9 +3,9 @@
 
 namespace OpenCVApp {
 
-	const std::string Paths::MODEL_DIR = ".\\model";
+	const std::string Paths::MODEL_DIR = "..\\model";
 	const std::string Paths::ALEXNET_DIR = MODEL_DIR + "\\AlexNet";
 	const std::string Paths::GOOGLENET_DIR = MODEL_DIR + "\\GoogLeNet";
-	const std::string Paths::IMAGE_DIR = ".\\image";
+	const std::string Paths::IMAGE_DIR = "..\\image";
 }
 
