e131b1ed49271cda777b7ad10e4d2856e3652396
houkiboshiComet <wish.on.broomystars@gmail.com>
2018-01-25 20:29:23 +0900

    Empty GoogLeNet Classifier implement

--- CV_DNN_Application/AlexNetClassifier.cpp
+++ CV_DNN_Application/AlexNetClassifier.cpp
@@ -14,27 +14,17 @@ namespace OpenCVApp {
 
 	/* 1. ニューラルネットワークを形成する。 */
 	void AlexNetClassifier::createNeuralNet() {
-		static const string prototxt 
-			= Paths::ALEXNET_DIR + "\\deploy.prototxt";
-		static const string model
-			= Paths::ALEXNET_DIR + "\\bvlc_alexnet.caffemodel";
-		
-		net = cv::dnn::readNetFromCaffe(prototxt, model);
+		/* IMPLEMENTS ME */
 	}
 	
 	/* 2. 入力層に画像をセットする。 */
 	void AlexNetClassifier::setImage(const cv::Mat* image) {
-		static const cv::Size cropSize(227, 227);
-		static const cv::Scalar averageColor( IMAGENET_MEAN_B, IMAGENET_MEAN_G, IMAGENET_MEAN_R);
-		cv::Mat blob = cv::dnn::blobFromImage(*image, 1, cropSize, averageColor, false, true);
-		net.setInput(blob, "data");
+		/* IMPLEMENTS ME */
 	}
 
 	/* 3. 出力層まで順伝播させる。( = 画像分類を行う。) */
 	void AlexNetClassifier::classify(cv::Mat* probabilities) {
-		cv::Mat layersOutput = net.forward("prob");
-		/* ポインタの指す行列の値が、順伝播によって変わるためDeep Copy */
-		*probabilities = layersOutput.clone();
+		/* IMPLEMENTS ME */
 	}
 
 }
